378e70bce3023dc2bba863302f45c76b5c2045e6
Vassil Guenoff <vguenoff@gmail.com>
2018-04-08 13:26:01 +0300

    Axios, Promises, and the Github API

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 import styled from 'styled-components';
 
+import { battle } from '../utils/api';
+
 export default class Results extends Component {
+    componentDidMount() {
+        const players = queryString.parse(this.props.location.search);
+        const { playerOneName, playerTwoName } = players;
+        battle([playerOneName, playerTwoName]).then(data => console.log(data));
+    }
+
     render() {
-        console.log(this.props);
-        console.log(this.props.location.search);
         return <StyledResults>Results</StyledResults>;
     }
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,56 @@
 import axios from 'axios';
 
+const id = 'Iv1.a5e06c0b0d771b77';
+const sec = '7d35315398bf0512603c71a2c49f32b3fe70909f';
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function getProfile(username) {
+    return axios.get(`https://api.github.com/users/${username}${params}`).then(user => user.data);
+}
+
+function getRepos(username) {
+    return axios
+        .get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+        .then(user => user.data);
+}
+
+function getStarCount(repos) {
+    return repos.reduce((count, repo) => count + repo.stargazers_count, 0);
+}
+
+function calculateScore(profile, repos) {
+    const followers = profile.followers;
+    const totalStars = getStarCount(repos);
+
+    return followers * 3 + totalStars;
+}
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData(player) {
+    return axios.all([getProfile(player), getRepos(player)]).then(data => {
+        const [profile, repos] = data;
+        return {
+            profile,
+            score: calculateScore(profile, repos),
+        };
+    });
+}
+
+function sortPlayers(players) {
+    return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+    return axios
+        .all(players.map(player => getUserData(player)))
+        .then(player => sortPlayers(player))
+        .catch(err => handleError(err));
+}
+
 export function fetchPopularRepos(language) {
     const source = axios.CancelToken.source();
 
